87f144aafe9d418db01caf58748e8483636c2d21
jynadal <nadaljeanyves@hotmail.fr>
2020-09-18 13:43:25 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    preventDefault();
+console.log('Started')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
